7da59a04bd5b547948cb96a2a503ac55da503f8a
sammy-jay <sammyjayden27@gmail.com>
2025-03-01 01:42:30 +0100

    layout update

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,8 +9,8 @@ import { Navbar } from "./components/Navbar";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Teros AI",
-  description: "Leading IT Solutions & Services Provider",
+  title: "Teros AI: Dominate Your Market With AI",
+  description: "Unlock explosive growth with AI-powered sales and customer support solutions. Generate high-quality leads, automate key processes, and transform your B2B business.",
 };
 
 export default function RootLayout({
